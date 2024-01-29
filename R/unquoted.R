7ff86254f3470043cfc49f511c244e72cd30494c
Filippo DallOlio <fdallolio@brocku.ca>
2024-01-29 17:11:37 -0500

    .

---
+++ R/unquoted.R
@@ -0,0 +1,12 @@
+#' Concatenate unquoted object names
+#'
+#' @param ... quoted or unquoted object names
+#'
+#' @return a character vector.
+#' @export
+#'
+cc <- function(...){
+  dots <- enquos(..., .named = TRUE)
+  names(dots)
+}
+
