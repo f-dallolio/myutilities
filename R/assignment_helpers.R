d8c6e4d4bd9d4183a09865dc14513d5df627a662
Filippo DallOlio <fdallolio@brocku.ca>
2024-01-31 16:38:19 -0500

    .

---
+++ R/assignment_helpers.R
@@ -0,0 +1,30 @@
+#' Assignment helpers
+#'
+#' @param x vactor of symbols for assignment. Same as lhs of `%<<-%`.
+#' @param value values to assign.
+#' @param env environment.
+#'
+#' @name assignment_helpers
+NULL
+#'
+#' @rdname assignment_helpers
+#' @export
+m_assign <- function(x, value, env = .GlobalEnv) {
+  tryCatch(zeallot:::multi_assign(substitute(x), value, env),
+           invalid_lhs = function(e) {
+             stop("invalid `%<-%` left-hand side, ", e$message,
+                  call. = FALSE
+             )
+           }, invalid_rhs = function(e) {
+             stop("invalid `%<-%` right-hand side, ", e$message,
+                  call. = FALSE
+             )
+           }
+  )
+}
+#'
+#' @rdname assignment_helpers
+#' @export
+`%<<-%` <- function() {
+  purrr:::partial(m_assign, env = .GlobalEnv)
+}
