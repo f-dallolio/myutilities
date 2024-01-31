ad15d5b642976ef49e68b3912f3ef9e24dfa4e7b
Filippo DallOlio <fdallolio@brocku.ca>
2024-01-31 17:48:34 -0500

    .

---
+++ R/empty_named_list.R
@@ -0,0 +1,19 @@
+#' Empty list
+#'
+
+#' @name empty_list_df
+NULL
+#'
+#' @rdname empty_list_df
+#' @export
+empty_named_list <- function(...){
+  .names <- rlang::enquos(..., .named = TRUE) |> names()
+  .data <- vector("list", length(.names))
+  structure(.data, .Names = .names)
+}
+#'
+#' @rdname empty_list_df
+#' @export
+empty_named_df <- function(...){
+  empty_named_list(...) |> as.data.frame()
+}
